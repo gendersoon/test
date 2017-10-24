d24afc2a168b7ccfe4317bdddd0b39df6b08d1df
icarreons <akatsuki96_ivan@hotmail.com>
2017-10-24 17:55:23 -0500

    cambio7

--- test_cosa3.py
+++ test_cosa3.py
@@ -5,6 +5,19 @@ class TestCake(unittest.TestCase):
 
     def testCandles(self):
         self.assertEqual(birthdayCakeCandles(5, [2, 2, 2, 2, 2]), 5)
+        def testHora(self):
+            # Primer camino
+            self.assertEqual(timeConversion("12:50:42AM"), "00:50:42")
+
+            # Segundo camino
+            self.assertEqual(timeConversion("05:12:56AM"), "05:12:56")
+
+            # Tercer camino
+            self.assertEqual(timeConversion("12:45:23PM"), "12:45:23")
+
+            # Cuarto camino
+            self.assertEqual(timeConversion("11:04:41PM"), "23:04:41")
+
 
 if __name__ == '__main__':
     unittest.main()
