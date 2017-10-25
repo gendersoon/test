4fdf2857c2039d13dc60b6958b0e1591302f6778
icarreons <akatsuki96_ivan@hotmail.com>
2017-10-25 14:58:59 -0500

    otro cambio4

--- cosa.py
+++ cosa.py
@@ -1,24 +1,18 @@
-#problema 1 : time-convertion
+#problema 2 : Cake-Candles
 import sys
 
-def timeConversion(s):
-    formato = s[-2:] #variable que solo me trae los ultimos 2 caracteres
-    th = s[:2] #variable que solo toma las horas (primeros 2 nums)
-    #ciclo que revisa el formato de la hora (AM/PM)
-    if formato == 'AM':
-        if  th == '12': #ciclo que comprueba la hora
-            return "00" + s[2:8]
+def birthdayCakeCandles(n, ar):
+    
+    ar.sort(reverse=True)
+
+    max = ar[0]
+    c = 0
+    for objeto in ar:
+
+        if max == objeto:
+            c = c + 1
+
         else:
-            return s[:-2] #regresa todo el string, menos el formato
-    #si no cumple con el ciclo anterior, solo se comprueba la hora
-    elif th == '12':
-            return s[:-2] #regresa todo el string, menos el formato
-    else:
-            i = int(th) + 12 #se transforma el string a int para efectuar la suma, y asignar el resultado a la var. i
-            return  str(i) + s[2:8] #la variable i se vuelve a transformar a string para poder hacer la concatencacion de min y seg
+            break
 
-
-
-s = input().strip()
-result = timeConversion(s)
-print(result)
+    return c
